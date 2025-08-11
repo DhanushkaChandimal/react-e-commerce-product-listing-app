a90016873d28b642fbf4e9dec8b89a9bebf3f07b
Dhanushka <dhanushkamcr@gmail.com>
2025-08-11 17:05:40 -0500

    styled product list

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -4,9 +4,13 @@ const PorductItem = ({item}) =>{
     return(
         <div className="item">
             <h2>{item.name}</h2>
-            <img src={`assets/${item.image_name}`} alt={`${item.name} image`} />
-            <p>${item.price}</p>
-            <p>{item.description}</p>
+            <div className='image-container'>
+                <img src={`assets/${item.image_name}`} alt={`${item.name} image`} />
+            </div>
+            <div className='text-container'>
+                <p><strong>${item.price}</strong></p>
+                <p>{item.description}</p>
+            </div>
         </div>
     )
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,14 @@
+import '../styles/productList.css'
 import PorductItem from './ProductItem'
 
 const PorductList = ({products}) =>{
     // console.log(products);
     return(
-        <>
+        <div className='container'>
             {products.map((value) => (
                 <PorductItem key={value.id} item={value}/>
             ))}
-        </>
+        </div>
     )
 }
 
