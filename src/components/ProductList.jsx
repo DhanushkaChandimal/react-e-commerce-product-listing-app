1e63abad126597e04f4b838f6289d772137a36ba
Dhanushka <dhanushkamcr@gmail.com>
2025-08-13 15:11:32 -0500

    Add dropdown to filter products by category

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import '../styles/productList.css'
 import PorductItem from './ProductItem'
 
 const PorductList = ({products}) =>{
     // console.log(products);
+    const [selectedCategory, setSelectedCategory] = useState("");
+    const [filteredProducts, setFilteredProducts] = useState(products);
+    const categories = [...new Set(products.map(p => p.category))];
+
+    const handleChange = (e) => {
+        setSelectedCategory(e.target.value);
+        setFilteredProducts(e.target.value
+            ? products.filter(p => p.category === e.target.value)
+            : products);
+        console.log(e.target.value)
+    }
+
     return(
-        <div className='container'>
-            {products.map((value) => (
-                <PorductItem key={value.id} item={value}/>
-            ))}
+        <div>
+            <div className='search-bar'>
+                <input type="text" />
+                <select value={selectedCategory} onChange={e => handleChange(e)}>
+                    <option value="">All</option>
+                    {categories.map((category, index) => (
+                        <option key={index} value={category}>{category}</option>
+                    ))}
+                </select>
+            </div>
+            <div className='container'>
+                {filteredProducts.map((value) => (
+                    <PorductItem key={value.id} item={value}/>
+                ))}
+            </div>
         </div>
     )
 }
