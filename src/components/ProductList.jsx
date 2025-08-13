38c34b1f3241de242d501b3e4a774b73334b0386
Dhanushka <dhanushkamcr@gmail.com>
2025-08-13 15:36:46 -0500

    Implemented search bar ro search by name, price or description

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,26 +1,44 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../styles/productList.css'
 import PorductItem from './ProductItem'
 
 const PorductList = ({products}) =>{
     // console.log(products);
     const [selectedCategory, setSelectedCategory] = useState("");
+    const [searchText, setSearchText] = useState("");
     const [filteredProducts, setFilteredProducts] = useState(products);
     const categories = [...new Set(products.map(p => p.category))].sort((a, b) => a.localeCompare(b));
 
-    const handleChange = (e) => {
-        setSelectedCategory(e.target.value);
-        setFilteredProducts(e.target.value
-            ? products.filter(p => p.category === e.target.value)
+    useEffect(()=>{
+        let categoryFilter = selectedCategory ? products.filter(p => p.category === selectedCategory) : products;
+        setFilteredProducts(searchText
+            ? categoryFilter.filter(
+                p => p.name.includes(searchText) || p.price.includes(searchText) || p.description.includes(searchText))
+            : categoryFilter);
+    },[products, searchText, selectedCategory]);
+
+    const handleCtegoryChange = (event) => {
+        setSelectedCategory(event.target.value);
+        setFilteredProducts(event.target.value
+            ? products.filter(p => p.category === event.target.value)
             : products);
-        console.log(e.target.value)
+        console.log(event.target.value)
+    }
+
+    const handleSearchChange = (event) => {
+        setSearchText(event.target.value);
+        setFilteredProducts(event.target.value
+            ? products.filter(p => p.category === event.target.value)
+            : products);
+        console.log(event.target.value)
     }
 
     return(
         <div>
+            {/* Optional features: bonus challenge */}
             <div className='search-bar'>
-                <input type="text" />
-                <select value={selectedCategory} onChange={e => handleChange(e)}>
+                <input type="text" value={searchText} onChange={handleSearchChange}/>
+                <select value={selectedCategory} onChange={handleCtegoryChange}>
                     <option value="">All</option>
                     {categories.map((category, index) => (
                         <option key={index} value={category}>{category}</option>
